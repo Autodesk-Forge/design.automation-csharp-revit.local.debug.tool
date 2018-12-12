b8b76bbe908ffe5702070481d2be9ae6dffe867a
Ashwin Shivashankar <ashwin.shivashankar@autodesk.com>
2018-12-12 16:08:07 -0500

    The app sucesfully raises the DAReady event. We're able to run an unmodified DeleteWalls on it. TODO ask for input regarding which file we're using, or ask for JSON input.

--- DesignAutomationHandler/DesignAutomationHandler.cs
+++ DesignAutomationHandler/DesignAutomationHandler.cs
@@ -12,12 +12,10 @@ namespace DesignAutomationHandler
    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
    class DesignAutomationHandlerApp : IExternalDBApplication
    {
-
-
-
       public ExternalDBApplicationResult OnStartup(Autodesk.Revit.ApplicationServices.ControlledApplication app)
       {
-         //DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+         // register for application initialized.
+         app.ApplicationInitialized += HandleApplicationInitializedEvent;
          return ExternalDBApplicationResult.Succeeded;
       }
 
@@ -26,10 +24,11 @@ namespace DesignAutomationHandler
          return ExternalDBApplicationResult.Succeeded;
       }
 
-      public void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+      public void HandleApplicationInitializedEvent(object sender, Autodesk.Revit.DB.Events.ApplicationInitializedEventArgs e)
       {
-
-         e.Succeeded = true;
+         Application app = sender as Application;
+         // when application ready is called, set design automation ready.
+         bool designAutomationResult = DesignAutomationBridge.SetDesignAutomationReady(app, "/path/goes/here"); // TODO take user input for app.
       }
    }
 }
