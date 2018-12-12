8eb6ba65b0168523a7e57a8bb9198099f5a32fbe
Ashwin Shivashankar <ashwin.shivashankar@autodesk.com>
2018-12-12 12:58:29 -0500

    Initial commit, nothing works.

---
+++ DesignAutomationHandler/DesignAutomationHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using DesignAutomationFramework;
+
+namespace DesignAutomationHandler
+{
+   [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+   [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+   class DesignAutomationHandlerApp : IExternalDBApplication
+   {
+
+
+
+      public ExternalDBApplicationResult OnStartup(Autodesk.Revit.ApplicationServices.ControlledApplication app)
+      {
+         //DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+         return ExternalDBApplicationResult.Succeeded;
+      }
+
+      public ExternalDBApplicationResult OnShutdown(Autodesk.Revit.ApplicationServices.ControlledApplication app)
+      {
+         return ExternalDBApplicationResult.Succeeded;
+      }
+
+      public void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+      {
+
+         e.Succeeded = true;
+      }
+   }
+}
