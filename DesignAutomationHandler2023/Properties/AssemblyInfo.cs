57b5b690f768f7b40ba59e8a9f29b4577af1acdf
kadamakash <akash.track@gmail.com>
2022-04-05 16:53:00 -0400

    corrections

--- DesignAutomationHandler2023/Properties/AssemblyInfo.cs
+++ DesignAutomationHandler2023/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("B11C8244-69F1-42D7-A1D2-98CA57A1BABC")]
+[assembly: Guid("F009623B-E123-4130-BFC7-F310BE56178E")]
 
 // Version information for an assembly consists of the following four values:
 //
